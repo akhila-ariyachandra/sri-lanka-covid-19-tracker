f60363f0a266d4ec193a77fcb1847af04a375abf
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-03-29 22:34:15 +0530

    Added more links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 import Typography from "@material-ui/core/Typography";
 const Link = dynamic(() => import("@material-ui/core/Link"));
 import GitHubIcon from "@material-ui/icons/GitHub";
+import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -31,16 +32,54 @@ const Layout: React.FunctionComponent = ({ children }) => {
       </Container>
 
       <Container maxWidth="md" component="footer" className={classes.footer}>
-        <Typography className={classes.text}>
-          Created by{" "}
-          <Link
-            href="https://akhilaariyachandra.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Akhila Ariyachandra
-          </Link>
-        </Typography>
+        <Box className={classes.text}>
+          <Typography>
+            Created by{" "}
+            <Link
+              href="https://akhilaariyachandra.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Akhila Ariyachandra
+            </Link>
+          </Typography>
+
+          <Typography>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <Link
+              href="https://nextjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Next.js
+            </Link>
+            {`, `}
+            <Link
+              href="https://material-ui.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Material-UI
+            </Link>
+            {`, `}
+            <Link
+              href="http://recharts.org/en-US/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Recharts
+            </Link>
+            {`, & `}
+            <Link
+              href="https://zeit.co/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ZEIT
+            </Link>
+          </Typography>
+        </Box>
 
         <Link
           href="https://github.com/akhila-ariyachandra/sri-lanka-covid-19-tracker"
