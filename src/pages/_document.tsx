a50e51e8b5e127d6c9661d70b6c481fb42abcbcd
akhila-ariyachandra <akhila_ariyachandra@live.com>
2020-04-23 15:51:20 +0530

    Updated Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,22 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" type="image/png" href="/virus.png" />
+
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-137864394-3"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-137864394-3');`,
+            }}
+          />
         </Head>
 
         <body>
